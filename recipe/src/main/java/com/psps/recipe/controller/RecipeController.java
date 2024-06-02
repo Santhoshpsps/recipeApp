e6e0be44c7b6b064c4554ba96dfbbab401308ade
Santhosh PSPS <santhoshpsp4@gmail.com>
2024-06-02 18:08:11 +0530

    Spring MVC Exception Handling

--- recipe/src/main/java/com/psps/recipe/controller/RecipeController.java
+++ recipe/src/main/java/com/psps/recipe/controller/RecipeController.java
@@ -1,14 +1,16 @@
 package com.psps.recipe.controller;
 
 import com.psps.recipe.commands.RecipeCommand;
+import com.psps.recipe.exceptions.NotFoundException;
 import com.psps.recipe.service.RecipeService;
 
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RecipeController {
@@ -55,4 +57,26 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+     public ModelAndView handle404(Exception exception){
+        log.error("Handling Not Found Exception!! ");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404Error");
+        modelAndView.addObject("exception",exception);
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleBadRequest(){
+        log.error("Handling Number Format Exception !!  ");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("BadRequestError");
+
+        return modelAndView;
+    }
+
 }
