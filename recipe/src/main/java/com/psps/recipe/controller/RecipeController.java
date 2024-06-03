7d6b0e21b8c23d7dd1a6dccb9bff81f79a8ef2d2
Santhosh PSPS <santhoshpsp4@gmail.com>
2024-06-03 18:10:59 +0530

    Using @ControllerAdvice to consolidate all exceptionHandlers for controllers.

--- recipe/src/main/java/com/psps/recipe/controller/RecipeController.java
+++ recipe/src/main/java/com/psps/recipe/controller/RecipeController.java
@@ -58,25 +58,8 @@ public class RecipeController {
         return "redirect:/";
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-     public ModelAndView handle404(Exception exception){
-        log.error("Handling Not Found Exception!! ");
-        log.error(exception.getMessage());
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("404Error");
-        modelAndView.addObject("exception",exception);
-        return modelAndView;
-    }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleBadRequest(){
-        log.error("Handling Number Format Exception !!  ");
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("BadRequestError");
 
-        return modelAndView;
-    }
+
 
 }
